6938bea389aad849e059e1d899e0a4ada94ab59f
yu-ya4 <yuya4.matsumura@gmail.com>
2019-04-30 04:59:56 +0900

    Add get_similarity()

--- simple_cf.py
+++ simple_cf.py
@@ -23,3 +23,26 @@ def jaccard_similarity(x, y):
     union = len(x.union(y))
     return float(intersection / union)
 
+
+# 過去の行動履歴に基づいた 2 ユーザの類似度を計算
+def get_similariy(user1, user2):
+
+    # ユーザの行動履歴
+    history1 = data[user1]
+    history2 = data[user2]
+
+    # 両方とものユーザに推薦されたアイテムの集合をとる。
+    recommended_items1 = set(history1.keys())
+    recommended_items2 = set(history2.keys())
+    recommended_both = recommended_items1.intersection(recommended_items2)
+
+    #両方ともに推薦されたアイテムがなければ類似度 0 とする。
+    if len(recommended_both) == 0:
+        return 0.0
+
+    # 両方とものユーザに推薦されていてかつ，購入したアイテムの集合をとる。
+    possitive_items1 = set([key for key, val in history1.items() if key in recommended_both and val == 1])
+    possitive_items2 = set([key for key, val in history2.items() if key in recommended_both and val == 1])
+
+    return jaccard_similarity(possitive_items1, possitive_items2)
+
