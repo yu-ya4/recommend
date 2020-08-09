c81cc6a0fe4e6df83cd0fdc957cfc5ed4d46c70e
yu-ya4 <yuya4.matsumura@gmail.com>
2020-08-10 00:23:52 +0900

    Add test_get_similarity()

--- tests/test_simple_cf.py
+++ tests/test_simple_cf.py
@@ -1,4 +1,8 @@
-from src.recommend.simple_cf import jaccard_similarity, get_similarity, get_recommends
+from src.recommend.simple_cf import (
+    jaccard_similarity,
+    get_similarity,
+    get_recommends,
+)
 
 
 def test_jaccard_similarity() -> None:
@@ -16,3 +20,19 @@ def test_jaccard_similarity() -> None:
 
     res3 = jaccard_similarity(x=a, y=c)
     assert res3 == 0.5, "The similarity between {1, 2, 3} and {2, 3, 4} must be 0.5."
+
+
+def test_get_similarity() -> None:
+    res1 = get_similarity(user1="A", user2="A")
+    assert res1 == 1.0, "The similarity between the same users must be 1.0."
+
+    res2 = get_similarity(user1="A", user2="B")
+    assert (
+        res2 == 0.0
+    ), "The similarity between users that have no common interests must be 1.0."
+
+    res3 = get_similarity(user1="A", user2="C")
+    assert res3 == 1.0, "The similarity between userA and userC must be 1.0."
+
+    res4 = get_similarity(user1="A", user2="D")
+    assert res4 == 0.25, "The similarity between userA and userD must be 0.25."
