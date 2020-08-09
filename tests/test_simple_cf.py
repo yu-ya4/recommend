8336146c3559f9ccb9f1a676026fa980f571733d
yu-ya4 <yuya4.matsumura@gmail.com>
2020-08-09 23:31:08 +0900

    Add test_jaccard_similarity

--- tests/test_simple_cf.py
+++ tests/test_simple_cf.py
@@ -1,5 +1,18 @@
-from src.recommend.simple_cf import jaccard_similarity,get_similarity, get_recommends
+from src.recommend.simple_cf import jaccard_similarity, get_similarity, get_recommends
 
-def test_sample_function1():
-    print("test_sample_function1")
-    assert True
+
+def test_jaccard_similarity() -> None:
+    a = set([1, 2, 3])
+    b = set([4, 5])
+    c = set([2, 3, 4])
+
+    res1 = jaccard_similarity(x=a, y=a)
+    assert res1 == 1.0, "The similarity between the same sets must be 1.0."
+
+    res2 = jaccard_similarity(x=a, y=b)
+    assert (
+        res2 == 0.0
+    ), "The similarity between sets that do not have any common elements must be 0.0."
+
+    res3 = jaccard_similarity(x=a, y=c)
+    assert res3 == 0.5, "The similarity between {1, 2, 3} and {2, 3, 4} must be 0.5."
