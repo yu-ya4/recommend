5089d9653fc6d779498444a88145c734fd66cc6f
yu-ya4 <yuya4.matsumura@gmail.com>
2020-09-22 02:18:25 +0900

    Change unzipped filename to dataset name

--- src/recommend/data_loader.py
+++ src/recommend/data_loader.py
@@ -8,12 +8,11 @@ dir_path = "./datasets"
 os.makedirs(dir_path, exist_ok=True)
 
 
-if __name__ == "__main__":
-    dataset_name = "ml-100k"
+def load_data(dataset_name: str = "ml-100k") -> None:
     dataset_path = dir_path + "/" + dataset_name
     if os.path.exists(dataset_path):
         print("dataset already exists.")
-        exit()
+        return
 
     zip_file_name = dataset_name + ".zip"
     url = base_url + zip_file_name
@@ -21,4 +20,11 @@ if __name__ == "__main__":
 
     with zipfile.ZipFile(dir_path + "/" + zip_file_name) as zip_file:
         zip_file.extractall(dir_path)
+        unzip_dir_name = zip_file.namelist()[0].split("/")[0]
+        if unzip_dir_name != dataset_name:
+            os.rename(dir_path + "/" + unzip_dir_name, dataset_path)
     os.remove(dir_path + "/" + zip_file_name)
+
+
+if __name__ == "__main__":
+    load_data(dataset_name="ml-10m")
